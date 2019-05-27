341b4351b92d7f2c304757179e4e57c3de3df628
johnjohncarter <chachat_2015@hotmail.com>
2019-05-27 09:20:56 +0700

    add env connect

--- config/database.js
+++ config/database.js
@@ -4,11 +4,15 @@ let database_conection = process.env.DB_CONNECTION;
 let database_host = process.env.DB_HOST;
 let database_port = process.env.DB_PORT;
 let database_name = process.env.DB_DATABASE;
+let database_username = process.env.DB_USERNAME;
+let database_password = process.env.DB_PASSWORD;
 
 module.exports = {
     connection: function () {
         mongoose.connect(
             database_conection + '://'
+            + database_username + ':'
+            + database_password + '@'
             + database_host + ':'
             + database_port + '/'
             + database_name
@@ -17,7 +21,7 @@ module.exports = {
         let database = mongoose.connection;
 
         database.once('open', function () {
-            console.log('connect to mongo_bd successfully!!!')
+            console.log('connect database mongo_bd successfully!!!')
         });
 
         database.on('error', function (err) {
